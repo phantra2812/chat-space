21f93f27099a2831e1fcf50ef3267c86690d873a
phantra2812 <32453468+phantra2812@users.noreply.github.com>
2017-10-18 17:05:53 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,19 +1,18 @@
 class GroupsController < ApplicationController
-	def new
-		@group = Group.new
-	end
+  def new
+    @group = Group.new
+  end
 
-	def create
-		@group = Group.create(group_params)
-		if @group.save
-		   redirect_to :root, notice: 'グループ作成成功'
-		else render :new
-		end
+  def create
+    @group = Group.create(group_params)
+    if @group.new
+      redirect_to :root, notice: 'グループ作成成功'
+    else render :new
+  end
+end
 
-	end
-
-	private
-	def group_params
-		params.require(:group).permit(:name)
-	end
+private
+  def group_params
+      params.require(:group).permit(:name)
+  end
 end
