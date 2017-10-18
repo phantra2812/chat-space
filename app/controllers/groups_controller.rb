8bb3250e6c221205b4cd2146d642f0408f7140e2
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-10-18 15:50:12 +0900

    Edit routes, define create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,19 @@
 class GroupsController < ApplicationController
+	def new
+		@group = Group.new
+	end
+
+	def create
+		@group = Group.create(group_params)
+		if @group.save
+		   redirect_to :root, notice: 'グループ作成成功'
+		else render :new
+		end
+
+	end
+
+	private
+	def group_params
+		params.require(:group).permit(:name)
+	end
 end
