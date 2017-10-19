a5c9a647185b9a03c19f3e20e96bcdbe319de489
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-10-19 18:44:27 +0900

    Modify_second

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,29 +3,16 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-	def create
-		@group = Group.new(group_params)
-		if @group.save
-		   redirect_to :root, notice: 'グループ作成成功'
-		else render :new
-		end
-	end
-
-	private
-	def group_params
-		params.require(:group).permit(:name, user_ids: [])
-	end
-
   def create
-    @group = Group.create(group_params)
-    if @group.new
+    @group = Group.new(group_params)
+    if @group.save
       redirect_to :root, notice: 'グループ作成成功'
     else render :new
     end
   end
 
-  private
-  def group_params
-      params.require(:group).permit(:name)
-  end
+	private
+	def group_params
+		params.require(:group).permit(:name, user_ids: [])
+	end
 end
