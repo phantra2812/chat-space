38b41f59443258c5570078ec19c1d8650f94f351
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-10-20 14:31:08 +0900

    Create-Edit-funtion-group-chat

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,8 +11,24 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = find_group
+  end
+
+  def update
+    @group = find_group
+    if @group.update(group_params)
+      redirect_to :root, notice: 'グループ更新成功'
+    else render :edit
+    end
+  end
+
 private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def find_group
+    Group.find(params[:id])
   end
 end
