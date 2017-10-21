ca0395d1b2a8bae55470c8ee531606a43e865b17
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-10-21 13:03:32 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:edit,:update]
   resources :groups, only: [:new, :create, :edit, :update]
-  resources :groups
+  resources :groups, except: [:destroy] do
+  	resources :messages, only: [:index, :create]
+  end
 end
