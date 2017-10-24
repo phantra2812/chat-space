3962e0eae4f73a058c0dca6648abff29e04ddc49
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-10-24 13:34:49 +0900

    Add Association into message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ApplicationRecord
 	belongs_to :user
 	belongs_to :group
+	validates :body_or_image, presence: true
+
+	mount_uploader :image, ImageUploader
+
+	private
+	def body_or_image
+		body.presence or image.presence
+	end
 end
