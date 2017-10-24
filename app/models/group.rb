c2704a30563e1ca32ea647f71b7922739c855aa8
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-10-24 15:06:14 +0900

    Display messages in the sider bar

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,17 @@ class Group < ApplicationRecord
 	has_many :group_users
 	has_many :messages
     validates :name, presence: true
+
+def last_messages
+  if self.messages.last.blank?
+    "まだメッセージはありません"
+  else
+    if self.messages.last.body.blank?
+      "画像が登録されました"
+    else
+     self.messages.last.body
+    end
+  end
+  end
+
 end
