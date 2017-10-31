0a74018da077cc63914d3e1894a6d13c4f3b066a
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-10-31 20:43:26 +0900

    Revert "Edit routes"
    This reverts commit 2c11304693b7c3f6f2d794cf3a8665099918581a.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
-
-	def index
-		@users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-		respond_to do |format|
-			format.html
-			format.json
-	end
-end
-
 	def edit
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:index, :edit,:update]
+  resources :users, only: [:edit,:update] do
+    collection do
+      get 'search'
+    end
+  end
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+  	resources :messages, only: [:index, :create]
   end
 end
