663fb33eb4987a7773a63b746851476c1184f982
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-12-04 15:55:16 +0900

    create group_users migration

--- db/migrate/20171204065342_group_users.rb
+++ db/migrate/20171204065342_group_users.rb
@@ -1,4 +1,4 @@
-class CreateGroupUsers < ActiveRecord::Migration[5.0]
+class GroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
       t.references :user, foreign_key: true, null: false
